feae44831c02ef8ca4e4c664dbe4dc6752f1174b
NourEl-Dien Nwara <noureldiennwara312@gmail.com>
2025-05-01 18:56:41 +0300

    Add files via upload

---
+++ GHR2_SWD6_S2_1/src/test/java/Register/Enter_All_data_correct.java
@@ -0,0 +1,68 @@
+package Register;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Random;
+
+public class Enter_All_data_correct {
+    public static void main (String [] args) {
+
+        // generate random number to choose male or female
+        Random random = new Random();
+        int gender = random.nextInt(2);
+
+        // this variable to check the Register valid or not valid
+        String ExpectedResults = "Your registration completed";
+        String ActualResults;
+
+        // this variable to store the data that well use in test
+        String first_name = "FTest";
+        String last_name = "LTest";
+        String email = "testuser" + System.currentTimeMillis() + "@example.com";
+        String password = "test123$";
+        String companyName = "test account";
+
+        // create webDriver to start test
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+
+        // Enter all data to Register
+        if(gender == 0)
+        {
+            driver.findElement(By.id("gender-male")).click();
+        }
+        else if (gender == 1)
+        {
+            driver.findElement(By.id("gender-female")).click();
+        }
+
+        driver.findElement(By.id("FirstName")).sendKeys(first_name);
+        driver.findElement(By.id("LastName")).sendKeys(last_name);
+        driver.findElement(By.id("Email")).sendKeys(email);
+
+        driver.findElement(By.id("Company")).sendKeys(companyName);
+
+        driver.findElement(By.id("Newsletter")).click();
+
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+
+        driver.findElement(By.id("register-button")).click();
+
+        // check the Register valid or not valid
+        ActualResults = driver.findElement(By.className("result")).getText();
+        if (ExpectedResults == ActualResults )
+        {
+            System.out.println("Register correct");
+        }
+        else
+        {
+            System.out.println("Register not correct");
+        }
+
+    }
+}
